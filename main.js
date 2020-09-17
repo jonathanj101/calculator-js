90dc7f600f70c114dd1b5026f49cc136d7957a10
jonathanj101 <jonatjimen@gmail.com>
2020-09-16 21:00:01 -0400

    separete functionnality for each operations!!
    separete functionnality for each operations!!

--- main.js
+++ main.js
@@ -2,36 +2,106 @@ document.addEventListener('DOMContentLoaded', () => {
     display()
 })
 
-const isDecimal = false
 const result = document.querySelector("#result")
 const prevResult = document.querySelector("#top-result")
 const operators = document.querySelectorAll("#operator")
 const insertBtns = document.querySelectorAll("#num")
+const divBtns = document.querySelectorAll('div.btns')
 
+
+let number;
+let number2
+let operator;
 
 function display() {
-    for (i = 0; i < insertBtns.length; i++) {
-        insertBtns[i].addEventListener('click', (e) => {
-            let parsed = parseInt(result.value + e.target.value)
-            result.value = parsed
-            if (e.target.value === '.') {
-                result.value = parsed + e.target.value
-            }
+    insertBtns.forEach(btns => {
+        btns.addEventListener('click', btn => {
+
+            number = btn.target.value
+
+            if (number.includes('.') && result.value.includes('.')) return;
+            result.value += number
         })
-    }
-    for (i = 0; i < operators.length; i++) {
-        operators[i].addEventListener('click', (e) => {
-            if (e.target.value !== "C" && e.target.value !== "=" && e.target.value !== "<") {
-                result.value = e.target.value
-            } else {
-                result.value != e.target.value
-                // prevResult.value = result.value
-            }
-
-        })
-    }
-
-
+    })
+    calculate()
 }
 
-// function 
+
+function calculate() {
+    operators.forEach(operation => {
+        operation.addEventListener('click', oper => {
+            operator = oper.target.value
+            switch (operator) {
+                case "C":
+                    clear(result)
+                    break;
+
+                case '<':
+                    backspace(result)
+                    break;
+
+                case '+':
+                    if (operator.includes("+") && result.value.includes("+")) return;
+                    result.value += operator
+
+                    break;
+
+                case '-':
+                    if (operator.includes("-") && result.value.includes("-")) return;
+                    result.value += operator
+
+                case '*':
+                    if (operator.includes("*") && result.value.includes("*")) return;
+                    result.value += operator
+                    break;
+
+                case "/":
+                    if (operator.includes("/") && result.value.includes("/")) return;
+                    result.value += operator
+                    break;
+
+                default:
+                    break;
+            }
+        })
+    })
+}
+
+function clear(str) {
+    str.value = "0"
+}
+
+function backspace(str) {
+    str.value = str.value.substr(0, str.value.length - 1)
+    if (str.value === '') {
+        result.value = 0
+    }
+}
+
+
+function addition(num, num2) {
+    num = parseFloat(num)
+    num2 = parseFloat(num2)
+    let total = num + num2
+    console.log(num, num2, total)
+    console.log(typeof (num), typeof (num2), typeof (total))
+    return total
+}
+
+function multiply(num, num2) {
+    num = parseFloat(num)
+    num2 = parseFloat(num2)
+    return total = num * num2
+}
+
+function devide(num, num2) {
+    num = parseFloat(num)
+    num2 = parseFloat(num2)
+    return total
+}
+
+function substract(num, num2) {
+    num = parseFloat(num)
+    num2 = parseFloat(num2)
+    return total = num - num2
+}
